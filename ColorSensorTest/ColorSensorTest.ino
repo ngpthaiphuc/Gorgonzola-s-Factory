676aa61bcc78ed03400d447f96a3a4763c13ad46
Thai <ngpthaiphuc@gmail.com>
2019-11-17 17:40:54 -0500

    [ADD] Buttons System added with some testing for the speaker.

--- ColorSensorTest/ColorSensorTest.ino
+++ ColorSensorTest/ColorSensorTest.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <Servo.h>
 #include "Adafruit_TCS34725.h"
 //#include "Adafruit_TCS34725softi2c.h"
 //
@@ -10,10 +11,22 @@ const int RED_PIN = 3;
 const int GREEN_PIN = 5;
 const int BLUE_PIN = 6;
 
+const int SERVO_PIN = 11;
+Servo servo;
+
 //Color Sensor Pins for Power
-const int SENSOR_A = 8;
-const int SENSOR_B = 9;
-const int SENSOR_C = 10;
+//const int SENSOR_A = 8;
+//const int SENSOR_B = 9;
+//const int SENSOR_C = 10;
+
+//Color Sensor Addresses
+const uint8_t SENSOR_A = 0x29;
+const uint8_t SENSOR_B = 0x30;
+const uint8_t SENSOR_C = 0x31;
+
+Adafruit_TCS34725 tcs_A = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+Adafruit_TCS34725 tcs_B = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+Adafruit_TCS34725 tcs_C = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);  
 
 boolean isCorrect_R = false;
 boolean isCorrect_G = false;
@@ -26,31 +39,65 @@ void setup(){
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
 
-  pinMode(SENSOR_A, OUTPUT);
-  pinMode(SENSOR_B, OUTPUT);
-  pinMode(SENSOR_C, OUTPUT);
+  servo.attach(SERVO_PIN);
+
+//Sensor Pins Version
+//  pinMode(SENSOR_A, OUTPUT);
+//  pinMode(SENSOR_B, OUTPUT);
+//  pinMode(SENSOR_C, OUTPUT);
+
+//Sensor Address Version
+  if(tcs_A.begin(SENSOR_A)){
+    Serial.println("Sensor A is activated!");
+  } else{
+    Serial.println("Sensor A not found");
+  }
+  if(tcs_B.begin(SENSOR_B)){
+    Serial.println("Sensor B is activated!");
+  } else{
+    Serial.println("Sensor B not found");
+  }
+  if(tcs_C.begin(SENSOR_C)){
+    Serial.println("Sensor C is activated!");
+  } else{
+    Serial.println("Sensor C not found");
+  }
 }
 
 void loop(){
   feedbackLED();
-  
-  for(int i = 0; i < 5; i++){
-    colorSensorCheck(SENSOR_A, "R");
-  }
-  delay(500);
-  digitalWrite(SENSOR_A, LOW);
 
+  if(isCorrect_R){
+    servo.write(160);
+    delay(1000);
+  } else {
+    servo.write(20);
+    delay(1000);
+  }
+  
+//Sensor Address Version
+  colorSensorCheck(tcs_A, "R");
+//  colorSensorCheck(tcs_B, "G");
+//  colorSensorCheck(tcs_C, "B");
+
+//Sensor Pins Version  
+//  for(int i = 0; i < 5; i++){
+//    colorSensorCheck(SENSOR_A, "R");
+//  }
+//  delay(500);
+//  digitalWrite(SENSOR_A, LOW);
+//
 //  for(int i = 0; i < 5; i++){
 //    colorSensorCheck(SENSOR_B, "G");
 //  }
 //  delay(500);
 //  digitalWrite(SENSOR_B, LOW);
-  
-  for(int i = 0; i < 5; i++){
-    colorSensorCheck(SENSOR_C, "B");
-  }
-  delay(500);
-  digitalWrite(SENSOR_C, LOW);
+//    
+//  for(int i = 0; i < 5; i++){
+//    colorSensorCheck(SENSOR_C, "B");
+//  }
+//  delay(500);
+//  digitalWrite(SENSOR_C, LOW);
 }
 
 void feedbackLED(){
@@ -68,115 +115,136 @@ void feedbackLED(){
     digitalWrite(RED_PIN, LOW);
   }
   if(isCorrect_G == true){
-//    digitalWrite(GREEN_PIN, HIGH);
-//    delay(50);
-//    digitalWrite(GREEN_PIN, LOW);
-//    delay(50);
-//    digitalWrite(GREEN_PIN, HIGH);
-//    delay(50);
-//    digitalWrite(GREEN_PIN, LOW);
-//    delay(50);
     digitalWrite(GREEN_PIN, HIGH);
   } else{
     digitalWrite(GREEN_PIN, LOW);
   }
   if(isCorrect_B == true){
-//    digitalWrite(BLUE_PIN, HIGH);
-//    delay(50);
-//    digitalWrite(BLUE_PIN, LOW);
-//    delay(50);
-//    digitalWrite(BLUE_PIN, HIGH);
-//    delay(50);
-//    digitalWrite(BLUE_PIN, LOW);
-//    delay(50);
     digitalWrite(BLUE_PIN, HIGH);
   } else{
     digitalWrite(BLUE_PIN, LOW);
   }
 }
 
-void colorSensorCheck(int sensorPin, String color){
-  Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+//Sensor Addresses Version
+void colorSensorCheck(Adafruit_TCS34725 sensor, String color){
+  float red, green, blue;
 
-  digitalWrite(sensorPin, HIGH);
+  sensor.setInterrupt(false);  // turn on the sensor LED
 
-  if(tcs.begin()){
-    float red, green, blue;
+  delay(60);  // takes 50ms to read
 
-    tcs.setInterrupt(false);  // turn on the sensor LED
+  sensor.getRGB(&red, &green, &blue);
   
-    delay(60);  // takes 50ms to read
-  
-    tcs.getRGB(&red, &green, &blue);
-    
-    tcs.setInterrupt(true);  // turn off the sensor LED
-  
-    if(color == "R"){
-      if(red > green && red > blue && red > 135){
-        isCorrect_R = true;
-        Serial.println("Red");
-      } else {
-        isCorrect_R = false;
-      }
-    } else if(color == "G"){
-      if(green > red && green > blue){
-        isCorrect_G = true;
-        Serial.println("Green");
-      } else {
-        isCorrect_G = false;
-      }
-    } else if(color == "B"){
-      if(blue > red && blue > green){
-        isCorrect_B = true;
-        Serial.println("Blue");
-      } else {
-        isCorrect_B = false;
-      }
+  sensor.setInterrupt(true);  // turn off the sensor LED
+
+  if(color == "R"){
+    if(red > green && red > blue && red > 135){
+      isCorrect_R = true;
+      Serial.println("Red");
+    } else {
+      isCorrect_R = false;
     }
-  } else{
-    Serial.println("Sensor not detected :(");
+  } else if(color == "G"){
+    if(green > red && green > blue){
+      isCorrect_G = true;
+      Serial.println("Green");
+    } else {
+      isCorrect_G = false;
+    }
+  } else if(color == "B"){
+    if(blue > red && blue > green){
+      isCorrect_B = true;
+      Serial.println("Blue");
+    } else {
+      isCorrect_B = false;
+    }
   }
 }
 
-void colorSensorTest(int sensorPin, String sensorName){
-  Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
-
-  digitalWrite(sensorPin, HIGH);
-
-  if(tcs.begin()){
-    float red, green, blue;
-  
-    tcs.setInterrupt(false);  // turn on LED
-  
-    delay(60);  // takes 50ms to read
-  
-    tcs.getRGB(&red, &green, &blue);
-    
-    tcs.setInterrupt(true);  // turn off LED
-  
-    Serial.print("R:\t"); Serial.print(int(red)); 
-    Serial.print("\tG:\t"); Serial.print(int(green)); 
-    Serial.print("\tB:\t"); Serial.print(int(blue));
-    Serial.print("\n");
-    
-//    if(red > green && red > blue && red > 135){
-//      digitalWrite(RED_PIN, HIGH);
-//      digitalWrite(BLUE_PIN, LOW);
-//      digitalWrite(GREEN_PIN, LOW);
-//    } else if(blue > red && blue > green){
-//      digitalWrite(BLUE_PIN, HIGH);
-//      digitalWrite(RED_PIN, LOW);
-//      digitalWrite(GREEN_PIN, LOW);
-//    } else if (green > red && green > blue){
-//      digitalWrite(GREEN_PIN, HIGH);
-//      digitalWrite(RED_PIN, LOW);
-//      digitalWrite(BLUE_PIN, LOW);
-//    } else{
-//      digitalWrite(RED_PIN, LOW);
-//      digitalWrite(BLUE_PIN, LOW);
-//      digitalWrite(GREEN_PIN, LOW);
+//Sensor Pins Version
+//void colorSensorCheck(int sensorPin, String color){
+//  Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+//
+//  digitalWrite(sensorPin, HIGH);
+//
+//  if(tcs.begin()){
+//    float red, green, blue;
+//
+//    tcs.setInterrupt(false);  // turn on the sensor LED
+//  
+//    delay(60);  // takes 50ms to read
+//  
+//    tcs.getRGB(&red, &green, &blue);
+//    
+//    tcs.setInterrupt(true);  // turn off the sensor LED
+//  
+//    if(color == "R"){
+//      if(red > green && red > blue && red > 135){
+//        isCorrect_R = true;
+//        Serial.println("Red");
+//      } else {
+//        isCorrect_R = false;
+//      }
+//    } else if(color == "G"){
+//      if(green > red && green > blue){
+//        isCorrect_G = true;
+//        Serial.println("Green");
+//      } else {
+//        isCorrect_G = false;
+//      }
+//    } else if(color == "B"){
+//      if(blue > red && blue > green){
+//        isCorrect_B = true;
+//        Serial.println("Blue");
+//      } else {
+//        isCorrect_B = false;
+//      }
 //    }
-  } else {
-    Serial.println(sensorName + " not found... check connections!");
-  }
-}
+//  } else{
+//    Serial.println("Sensor not detected :(");
+//  }
+//}
+//
+//void colorSensorTest(int sensorPin, String sensorName){
+//  Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+//
+//  digitalWrite(sensorPin, HIGH);
+//
+//  if(tcs.begin()){
+//    float red, green, blue;
+//  
+//    tcs.setInterrupt(false);  // turn on LED
+//  
+//    delay(60);  // takes 50ms to read
+//  
+//    tcs.getRGB(&red, &green, &blue);
+//    
+//    tcs.setInterrupt(true);  // turn off LED
+//  
+//    Serial.print("R:\t"); Serial.print(int(red)); 
+//    Serial.print("\tG:\t"); Serial.print(int(green)); 
+//    Serial.print("\tB:\t"); Serial.print(int(blue));
+//    Serial.print("\n");
+//    
+////    if(red > green && red > blue && red > 135){
+////      digitalWrite(RED_PIN, HIGH);
+////      digitalWrite(BLUE_PIN, LOW);
+////      digitalWrite(GREEN_PIN, LOW);
+////    } else if(blue > red && blue > green){
+////      digitalWrite(BLUE_PIN, HIGH);
+////      digitalWrite(RED_PIN, LOW);
+////      digitalWrite(GREEN_PIN, LOW);
+////    } else if (green > red && green > blue){
+////      digitalWrite(GREEN_PIN, HIGH);
+////      digitalWrite(RED_PIN, LOW);
+////      digitalWrite(BLUE_PIN, LOW);
+////    } else{
+////      digitalWrite(RED_PIN, LOW);
+////      digitalWrite(BLUE_PIN, LOW);
+////      digitalWrite(GREEN_PIN, LOW);
+////    }
+//  } else {
+//    Serial.println(sensorName + " not found... check connections!");
+//  }
+//}
